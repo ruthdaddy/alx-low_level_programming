1c5241c501079e84a9f053dabf56273afc986653
Chioma Chukwueke <chukwuekechioma@yahoo.com>
2022-09-28 11:49:50 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+nclude "main.h"
+
+/**
+ * _puts_recursion - Prints a string followed by newline
+ * @s: string to be printed
+ *
+ * Return: Always 0
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
